79695681f14de768d0f161f37638472ddb1229c1
Asen Lazarov <asen@smule.com>
2016-10-21 01:37:43 +0300

    fix gen-conf.sh and mention it in README

--- smgscripts/gen-conf.sh
+++ smgscripts/gen-conf.sh
@@ -2,12 +2,12 @@
 
 TARGET="$1"
 if [ "$TARGET" == "" ] ; then
-    echo "Usage: $0 <target> [<fqdn>] [output=/etc/smg/hosts/\$TARGET.yml] [source=smgconf/templ-linux.yml]"
+    echo "Usage: $0 <target> [<fqdn>] [output=/etc/smg/hosts/\$TARGET.yml] [source=smgconf/templ-linux-pf.yml]"
     exit 1
 fi
 FQDN=${2:-"$TARGET"}
 OUTPUT=${3:-"/etc/smg/linux/$TARGET.yml"}
-SOURCE=${4:-"smgconf/templ-linux.yml"}
+SOURCE=${4:-"smgconf/templ-linux-pf.yml"}
 
 sed "s/localhost.localdomain/$FQDN/g" $SOURCE > $OUTPUT.tmp
 sed "s/localhost/$TARGET/g" $OUTPUT.tmp > $OUTPUT
