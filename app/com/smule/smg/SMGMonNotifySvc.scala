4decf96decf5997457c09b0956acc1d79aa3a821
Asen Lazarov <asen@smule.com>
2016-12-07 16:30:03 +0200

    do not pass message notification subject/body on the command line

--- app/com/smule/smg/SMGMonNotifySvc.scala
+++ app/com/smule/smg/SMGMonNotifySvc.scala
@@ -40,7 +40,7 @@ case class SMGMonNotifyCmd(id:String, command: String, timeoutSec: Int) {
 
   def alert(severity: SMGMonNotifySeverity.Value, alertKey: String, subjStr: String, bodyStr: String): Unit = {
     // TODO obsolete passing alert props via cmd line and use env instead
-    val cmdStr = s"$command '${severity.toString}' '${escapeSingleQuotes(alertKey)}' '${escapeSingleQuotes(subjStr)}' '${escapeSingleQuotes(bodyStr)}'"
+    val cmdStr = s"$command '${severity.toString}' '${escapeSingleQuotes(alertKey)}'"
     val myEnv = Map(
       "SMG_ALERT_SEVERITY" -> severity.toString,
       "SMG_ALERT_KEY" -> alertKey,
