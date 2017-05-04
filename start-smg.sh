2750eda2264d09c26e299151fbfd68cde1b2280c
Asen Lazarov <asen@smule.com>
2017-05-04 17:23:23 +0300

    updated start script to support providing local bind address

--- start-smg.sh
+++ start-smg.sh
@@ -2,8 +2,26 @@
 
 APP_HOME=`dirname $0`
 JVM_MEM=${1:-"8g"}
+HTTP_PORT=${2:-"9000"}
+BIND_ADDRESS=$3
+if [ "$BIND_ADDRESS" == "" ] ; then
+  BIND_OPT=""
+  JMX_BIND_OPT=""
+  BIND_STR=""
+else
+  BIND_STR=" bind=$BIND_ADDRESS"
+  BIND_OPT="-Dhttp.address=$BIND_ADDRESS"
+  JMX_BIND_OPT="-J-Djava.rmi.server.hostname=$BIND_ADDRESS -J-Dcom.sun.management.jmxremote.host=$BIND_ADDRESS"
+fi
 
 cd $APP_HOME
+
+PID_FILE=run/play.pid
+
+if [ -f $PID_FILE ] ; then
+    echo "Error: $PID_FILE exists. Please stop SMG or remove $PID_FILE manually if SMG is not running"
+    exit 1
+fi
 
 ALT_CONF=/etc/smg/app.conf
 if [ -f $ALT_CONF ] ; then
@@ -13,18 +31,19 @@ else
   APP_CONF=""
 fi
 
-JMX_OPTS="-J-Dcom.sun.management.jmxremote.port=9001 \
+JMX_OPTS="$JMX_BIND_OPT -J-Dcom.sun.management.jmxremote.port=9001 \
     -J-Dcom.sun.management.jmxremote.ssl=false \
     -J-Dcom.sun.management.jmxremote.authenticate=false"
 
 nohup bin/smg $APP_CONF -J-Xmx$JVM_MEM $JMX_OPTS \
     -Dplay.crypto.secret=fabe980f8f27865e11eeaf9e4ff4fc65 \
+    -Dhttp.port=$HTTP_PORT $BIND_OPT \
     -Dpidfile.path=run/play.pid \
     >logs/nohup.out 2>&1 &
 
 if [ "$?" == "0" ] ; then
-   echo "Started. Check $APP_HOME/logs/application.log"
+   echo "Started (mem=$JVM_MEM port=$HTTP_PORT$BIND_STR). \
+Check $APP_HOME/logs/nohup.out for errors and $APP_HOME/logs/application.log for progress"
 else
-   echo "Some error occured"
+   echo "Some error occurred"
 fi
-
