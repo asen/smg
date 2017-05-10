e26d65d0036c6d83d5408f974d435e54fb827858
Asen Lazarov <asen@smule.com>
2017-05-10 12:23:06 +0300

    sanity test for config parser

---
+++ test/ConfigParserSpec.scala
@@ -0,0 +1,26 @@
+import java.io.File
+
+import com.smule.smg.{SMGConfigParser, SMGLogger}
+import helpers.TestConfigSvc
+import org.junit.runner.RunWith
+import org.specs2.mutable.Specification
+import org.specs2.runner.JUnitRunner
+
+/**
+  * Created by asen on 5/9/17.
+  */
+@RunWith(classOf[JUnitRunner])
+class ConfigParserSpec extends Specification {
+  val log = SMGLogger
+
+  "SMGConfigParser.fetchCommandsTree" should {
+    "work" in {
+      val cp = new SMGConfigParser(log)
+      val c = cp.getNewConfig(Seq(), "smgconf/config-dev.yml")
+      log.info(c.humanDesc)
+      // TODO verify that globals and objects are parsed correctly
+      c.allErrors.size mustEqual 0
+    }
+  }
+
+}
