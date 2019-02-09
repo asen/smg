c23670e7331428354f8967fbee4cf433cfee988d
Asen Lazarov <asen@smule.com>
2019-02-09 18:00:36 +0200

    support separate iframe width and height for external cdash items

--- app/com/smule/smg/cdash/CDashItemExternal.scala
+++ app/com/smule/smg/cdash/CDashItemExternal.scala
@@ -3,8 +3,11 @@ package com.smule.smg.cdash
 case class CDashItemExternal(conf: CDashConfigItem, url: String) extends CDashItem {
   override val itemType: CDashItemType.Value = CDashItemType.External
 
+  lazy val frameHeightStr: String = conf.getDataStr("fheight").getOrElse(heightStr)
+  lazy val frameWidthStr: String = conf.getDataStr("fwidth").getOrElse(widthStr)
+
   override def htmlContent: String = {
-    <iframe height={heightStr} width={widthStr} src={ url } frameborder="0">{ url }</iframe>
+    <iframe height={frameHeightStr} width={frameWidthStr} src={ url } frameborder="0">{ url }</iframe>
   }.mkString
 
   override def linkUrl: Option[String] = Some(url)
