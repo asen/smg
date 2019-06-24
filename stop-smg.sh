7215f196efdfbf3210b77a76e1fb949c738f211b
Asen Lazarov <asen@smule.com>
2019-06-24 12:57:36 +0300

    wait for clean shutdown in stop-smg.sh

--- stop-smg.sh
+++ stop-smg.sh
@@ -11,3 +11,6 @@ if [ "$?" == "0" ] ; then
 else
    echo "Some error occured - possibly application not running"
 fi
+
+echo -n "Waiting for SMG to shut down cleanly (use kill -9 $PID to force): "
+while curl -sf localhost:9000/config/status >/dev/null 2>&1 ; do echo -n . ; sleep 1 ; done ; echo down
