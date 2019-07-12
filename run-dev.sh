a1e4a4b96fefe90fb9ee331aca639f6623c2a52f
Asen Lazarov <asen@smule.com>
2019-07-12 15:41:33 +0300

    bump up some accepted http tokens sizes

--- run-dev.sh
+++ run-dev.sh
@@ -4,6 +4,8 @@ export JAVA_OPTS="-XX:+UseParallelGC -Dcom.sun.management.jmxremote.port=9001 \
     -Dcom.sun.management.jmxremote.ssl=false \
     -Dcom.sun.management.jmxremote.authenticate=false \
     -Dakka.http.parsing.max-uri-length=2m \
-    -Dplay.server.netty.maxHeaderSize=65535"
+    -Dakka.http.parsing.max-header-value-length=2m \
+    -Dplay.server.akka.max-header-value-length=2m \
+    "
 
 exec sbt run
