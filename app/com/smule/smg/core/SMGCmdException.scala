9b86e9dad91e22070db06f3fbbf1812dfb4e3f1b
Asen Lazarov <asen@smule.com>
2020-09-27 01:05:13 +0300

    more verbose exception message - include stderr/stdout

--- app/com/smule/smg/core/SMGCmdException.scala
+++ app/com/smule/smg/core/SMGCmdException.scala
@@ -1,4 +1,4 @@
 package com.smule.smg.core
 
 case class SMGCmdException(cmdStr: String, timeoutSec: Int, exitCode: Int, stdout: String, stderr: String) extends
-  SMGFetchException(s"Command failed (exit code $exitCode): $cmdStr")
+  SMGFetchException(s"Command failed (exit code $exitCode): $cmdStr\nSTDOUT:\n${stdout}\nSTDERR:\n${stderr}")
