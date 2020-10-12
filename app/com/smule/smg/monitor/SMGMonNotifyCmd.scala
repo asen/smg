3eb9ffe9bb07900cf28c4ada8f8d41f95cc50adb
Asen Lazarov <asen@smule.com>
2020-10-12 16:24:42 +0300

    do not mess notification command arguments - SMG vars are passed by env

--- app/com/smule/smg/monitor/SMGMonNotifyCmd.scala
+++ app/com/smule/smg/monitor/SMGMonNotifyCmd.scala
@@ -10,14 +10,13 @@ case class SMGMonNotifyCmd(id:String, command: String, timeoutSec: Int) {
 
   def alert(severity: SMGMonNotifySeverity.Value, alertKey: String, subjStr: String, bodyStr: String): Unit = {
     // TODO obsolete passing alert props via cmd line and use env instead
-    val cmdStr = s"$command '${severity.toString}' '${escapeSingleQuotes(alertKey)}'"
     val myEnv = Map(
       "SMG_ALERT_SEVERITY" -> severity.toString,
       "SMG_ALERT_KEY" -> alertKey,
       "SMG_ALERT_SUBJECT" -> subjStr,
       "SMG_ALERT_BODY" -> bodyStr
     )
-    SMGCmd.runCommand(cmdStr, timeoutSec, myEnv)
+    SMGCmd.runCommand(command, timeoutSec, myEnv)
   }
 }
 
