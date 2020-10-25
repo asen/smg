0fe968e8215b802bec748ae314a61d43f1460351
Asen Lazarov <asen@smule.com>
2020-10-25 15:00:26 +0200

    support custom subject prefix in notif-mail.sh

--- smgscripts/notif-mail.sh
+++ smgscripts/notif-mail.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env bash
 
-# RCPTS=$1 - all args are passed on to mail
+SUBJ_PREFIX="[SMG]"
+if [ "$1" == "--sprefix" ] ; then
+  shift
+  SUBJ_PREFIX="$1"
+  shift
+fi
+
+# RCPTS=$1 - all remaining args are passed on to mail, including rcpts
 SEVERITY=$SMG_ALERT_SEVERITY #-$2  # one of  RECOVERY, ACKNOWLEDGEMENT, ANOMALY, WARNING, FAILED, CRITICAL, SMGERR, THROTTLED, UNTHROTTLED
 ALERT_KEY=$SMG_ALERT_KEY #$3
 SUBJ=$SMG_ALERT_SUBJECT #$4
 BODY=$SMG_ALERT_BODY #$5
 
 MYTIME=`date +%H:%M:%S`
-echo "$BODY" | mail -s "[SMG] $SUBJ ($MYTIME)" "$@"
+echo "$BODY" | mail -s "$SUBJ_PREFIX $SUBJ ($MYTIME)" "$@"
