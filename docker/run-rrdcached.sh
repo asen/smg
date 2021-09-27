c942574651f963d7827b9ed000dc5fc72a277210
Asen Lazarov <asen@smule.com>
2021-09-27 21:25:49 +0300

    try to keep the rrdcached container alive for longer than SMG

--- docker/run-rrdcached.sh
+++ docker/run-rrdcached.sh
@@ -1,3 +1,26 @@
 #!/bin/bash
 
-exec /usr/bin/rrdcached -g -l unix:/var/rrdtool/rrdcached/rrdcached.sock -m 664 -b /var/rrdtool/rrdcached "$@"
+SLEEP=${SMG_RRDCACHED_SLEEP_ON_TERM:-5}
+
+_term() {
+    echo "SMG-rrdcached: Caught SIGTERM signal! Sleeping for $SLEEP seconds to allow SMG to shutdown first"
+    sleep "$SLEEP"
+    kill "$child"
+    wait "$child"
+    echo "SMG-rrdcached: Exiting gracefully"
+    exit 0
+}
+
+/usr/bin/rrdcached -g -l unix:/var/rrdtool/rrdcached/rrdcached.sock -m 664 -b /var/rrdtool/rrdcached "$@" &
+ret=$?
+child=$!
+if [ "$ret" == "0" ] ; then
+    echo "SMG-rrdcached: Started."
+else
+    echo "SMG-rrdcached: Some error occurred ($ret)"
+    exit $ret
+fi
+
+trap _term SIGTERM
+
+wait "$child"
