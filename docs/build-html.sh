5dd9f33ab07a1d4bf5fee603412d9692693460d5
Asen Lazarov <asen@smule.com>
2021-10-09 03:04:25 +0300

    insert Back To Index links in htmls

--- docs/build-html.sh
+++ docs/build-html.sh
@@ -12,14 +12,20 @@ cd `dirname $0`
 PANDOC_CMD="pandoc -s -f markdown --template=templates/default.html"
 
 $PANDOC_CMD --toc --toc-depth=4 \
+    --metadata toc-title="Table of Contents" \
+    --metadata index-url=index.html \
     --metadata title='The History and Evolution of a Monitoring System' \
     History_and_Evolution.md > History_and_Evolution.html
 
 $PANDOC_CMD --toc --toc-depth=4 \
+    --metadata toc-title="Table of Contents" \
+    --metadata index-url=index.html \
     --metadata title='Smule Grapher (SMG) - Concepts Overview' \
     smg.md > smg.html
 
 $PANDOC_CMD --toc --toc-depth=4 \
+    --metadata toc-title="Table of Contents" \
+    --metadata index-url=index.html \
     --metadata title='Smule Grapher (SMG) - Configuration Reference' \
     smg-config.md > smg-config.html
 
@@ -34,7 +40,13 @@ mkdir -p howto/html
 for ht in `ls -1 howto/ | grep -v html` ; do
     ht_html=`echo $ht | $SED 's/\.md$/.html/g'`
     ttl=`echo $ht | $SED 's/\.md$//g' | $SED 's/_/ /g'`
-    $PANDOC_CMD --metadata pagetitle="SMG $ttl Howto" howto/$ht > howto/html/$ht_html
+    index_url=index.html
+    if [ "$ht_html" == "index.html" ] ; then
+        index_url=../index.html
+    fi
+    $PANDOC_CMD --metadata title-prefix="SMG Howto" --metadata pagetitle="$ttl" \
+        --metadata index-url=$index_url \
+        howto/$ht > howto/html/$ht_html
 done
 
 echo "build-html.sh: Done"
